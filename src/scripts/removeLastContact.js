4fec91ab8776e3aea68c8a744a51620f0044ed69
OlenaPanf <elena.el@ukr.net>
2024-07-14 22:46:52 +0300

    add

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -3,22 +3,21 @@ import { PATH_DB } from '../contacts/contacts.js';
 
 export const removeLastContact = async () => {
   try {
-    const data = await fs.readFile(PATH_DB, 'utf8'); // Читаємо вміст файлу
-    let contacts = JSON.parse(data); // Парсуємо отримані дані до масиву
-    // Перевіряємо, чи існує хоча б один елемент у масиві
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    let contacts = JSON.parse(data);
+
     if (contacts.length > 0) {
-      contacts.pop(); // Видаляємо останній контакт
-      // Записуємо змінений масив назад у файл
+      contacts.pop(); //видаляю
       await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
       console.log('Last contact removed');
-      return true; // Повертаємо результат операції
+      return true;
     } else {
       console.log('No contacts to remove.');
       return false;
     }
   } catch (error) {
     console.error('Error removing last contact:', error);
-    throw error; // Ви можете вибрати, чи хочете повертати помилку або не робити цього
+    throw error;
   }
 };
 
